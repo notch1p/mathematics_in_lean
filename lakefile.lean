34c4041db2af06ea83e2eb192731717646dae166
Evan Gao <notch1p2004@outlook.com>
2024-05-29 17:58:06 +0800

    c02s03

--- lakefile.lean
+++ lakefile.lean
@@ -6,10 +6,7 @@ package mil where
     ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
     ⟨`autoImplicit, false⟩,
     ⟨`relaxedAutoImplicit, false⟩]
-  -- moreLinkArgs := #[
-  --   "-L./.lake/packages/LeanCopilot/.lake/build/lib",
-  --   "-lctranslate2"
-  -- ]
+
 @[default_target]
 lean_lib MIL where
 
