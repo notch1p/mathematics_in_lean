3327d348799ae5ece18b9befe32a62e5545c5427
Evan Gao <notch1p2004@outlook.com>
2024-08-08 17:48:01 +0800

    s04

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -39,17 +39,46 @@ example : min a b = min b a := by
     apply min_le_left
 
 example : max a b = max b a := by
-  sorry
+  apply ge_antisymm
+  repeat
+    apply max_le
+    apply le_max_right
+    apply le_max_left
+
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  sorry -- im too dumb
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  apply add_le_add_right
+  apply min_le_left
+  apply add_le_add_right
+  apply min_le_right
+
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  · apply aux
+  have h: min (a + c) (b + c) = min (a + c) (b + c) - c + c := by rw[sub_add_cancel]
+  rw[h]
+  apply add_le_add_right
+  rw[sub_eq_add_neg]
+  apply le_trans
+  apply aux
+  rw[add_neg_cancel_right,add_neg_cancel_right]
+
+
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+example : |a| - |b| ≤ |a - b| := by
+  have h: |a| - |b| = |a - b + b| - |b| := by rw[sub_add_cancel]
+  have h₁: |a - b + b| - |b| <= |a - b| + |b| - |b| := by
+    apply sub_le_sub_right
+    apply abs_add
+  have h₂: |a - b| = |a - b| + |b| - |b| := by rw[add_sub_cancel_right]
+  rw[h, h₂]
+  exact h₁
 end
 
 section
@@ -66,7 +95,16 @@ example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  repeat
+    apply dvd_add
+  · rw[<-mul_assoc]
+    apply dvd_mul_of_dvd_left
+    apply dvd_mul_left
+  apply dvd_mul_left
+  rw[pow_two]
+  apply dvd_mul_of_dvd_left
+  exact h
+
 end
 
 section
@@ -78,7 +116,12 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  apply Nat.dvd_antisymm
+  repeat
+    apply dvd_gcd
+    apply gcd_dvd_right
+    apply gcd_dvd_left
+
+
+
 end
-
-
