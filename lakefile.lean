6754561c0c0a7752becfa8c2ef2b8d8a81f04a7f
Evan Gao <notch1p2004@outlook.com>
2024-10-05 21:49:54 +0800

    finish? c02s04

--- lakefile.lean
+++ lakefile.lean
@@ -10,4 +10,4 @@ package mil where
 @[default_target]
 lean_lib MIL where
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4"@"master"
+require mathlib from git "https://github.com/leanprover-community/mathlib4"@"v4.12.0"
