5a553857f3d3b9086d5ad4e0955b64ebfe63d819
Evan Gao <notch1p2004@outlook.com>
2024-10-05 21:47:57 +0800

    finish s01 and s02

--- lakefile.lean
+++ lakefile.lean
@@ -6,9 +6,12 @@ package mil where
     ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
     ⟨`autoImplicit, false⟩,
     ⟨`relaxedAutoImplicit, false⟩]
-
+  -- moreLinkArgs := #[
+  --   "-L./.lake/packages/LeanCopilot/.lake/build/lib",
+  --   "-lctranslate2"
+  -- ]
 @[default_target]
 lean_lib MIL where
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4"@"master"
-
+-- require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "main"
