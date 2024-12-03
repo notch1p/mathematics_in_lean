46cf0d31673dc7b88334baad3fdf5564a1f6d69b
Evan Gao <notch1p2004@outlook.com>
2024-12-03 11:37:06 +0800

    switch c09 back

--- MIL.lean
+++ MIL.lean
@@ -23,12 +23,12 @@ import MIL.C06_Structures.S03_Building_the_Gaussian_Integers
 import MIL.C07_Hierarchies.S01_Basics
 import MIL.C07_Hierarchies.S02_Morphisms
 import MIL.C07_Hierarchies.S03_Subobjects
--- import MIL.C08_Groups_and_Rings.S01_Groups
--- import MIL.C08_Groups_and_Rings.S02_Rings
--- import MIL.C09_Linear_Algebra.S01_Vector_Spaces
--- import MIL.C09_Linear_Algebra.S02_Subspaces
--- import MIL.C09_Linear_Algebra.S03_Endomorphisms
--- import MIL.C09_Linear_Algebra.S04_Bases
+import MIL.C08_Groups_and_Rings.S01_Groups
+import MIL.C08_Groups_and_Rings.S02_Rings
+import MIL.C09_Linear_Algebra.S01_Vector_Spaces
+import MIL.C09_Linear_Algebra.S02_Subspaces
+import MIL.C09_Linear_Algebra.S03_Endomorphisms
+import MIL.C09_Linear_Algebra.S04_Bases
 import MIL.C10_Topology.S01_Filters
 import MIL.C10_Topology.S02_Metric_Spaces
 import MIL.C10_Topology.S03_Topological_Spaces
